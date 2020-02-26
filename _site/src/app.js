fd465a3db0237e1f8b6060983a4fd5d436f887b4
Isaiah <isaiah@parkgrades.com>
2020-02-25 17:39:53 -0700

    IDk

--- _site/src/app.js
+++ _site/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const scouty = require('./scouty');
+
 app.set('view engine', 'pug');
 
 const addView = (path, view, dataFunction) => {
@@ -10,41 +12,12 @@ const addView = (path, view, dataFunction) => {
     });
 };
 
-addView('/event/:key', 'event', async (req) => {
-    var allMatches = await tba.get(`/event/${req.params.key}/matches/simple`);
-    var teams = await tba.get(`/event/${req.params.key}/teams/simple`);
-    var matches = [];
-    var teamPointCont = await scouty.getPointCont(req.params.key.substr(4, 7) + req.params.key.substr(0, 4), teams);
-    var predictedScores = {};
-    if (allMatches) {
-        for (i = 0; i < allMatches.length; i++) {
-            if (allMatches[i].comp_level == "qm") {
-                matches.push(allMatches[i]);
-            }
-        }
-        matches.sort(function (a, b) {
-            return a.match_number - b.match_number
-        });
-        for (var i = 0; i < matches.length; i++) {
-            predictedScores[matches[i].key] = { 
-                red: +(teamPointCont[matches[i].alliances.red.team_keys[0]] + teamPointCont[matches[i].alliances.red.team_keys[1]] + teamPointCont[matches[i].alliances.red.team_keys[2]]).toFixed(1),
-                blue: +(teamPointCont[matches[i].alliances.blue.team_keys[0]] + teamPointCont[matches[i].alliances.blue.team_keys[1]] + teamPointCont[matches[i].alliances.blue.team_keys[2]]).toFixed(1)
-            }
-        }
-    }
-    if (teams) {
-        teams.sort(function (a, b) {
-            return a.team_number - b.team_number
-        });
-    }
-    return {
-        event: await tba.get(`/event/${req.params.key}/simple`),
-        teams: teams,
-        matches: matches,
-        predictions: await tba.get(`/event/${req.params.key}/predictions`),
-        predictedScores: predictedScores
-    };
+addView('/', 'index', async (req) => {
+    return{databases: await scouty.getDatabases()};
 });
 
+addView('/event/:db', 'event', async (req) => {
+    return{data: await scouty.getData(req.params.db)};
+});
 
 app.listen(4334);
